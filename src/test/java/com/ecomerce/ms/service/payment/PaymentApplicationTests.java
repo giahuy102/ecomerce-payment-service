a4c0cb6643d592dd67a6bc47535c856f33b33dd9
huyle <ghuy102@gmail.com>
2024-05-14 23:04:33 +0700

    Init project

---
+++ src/test/java/com/ecomerce/ms/service/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.ms.service.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
